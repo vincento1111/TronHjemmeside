c3400c8d18ac96dd7d1c2df98d3025d6e1130b17
Vincento1111 <vincento2009@hotmail.com>
2023-04-25 07:36:13 +0200

    added money count on shop

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminPanelService } from '../Services/AdminPanel.service';
+import { UserStatsService } from '../Services/UserStats.service';
 import { Items } from '../Interfaces/Items';
 import { Inventory } from '../Interfaces/Inventory';
 @Component({
@@ -12,14 +13,18 @@ export class ShopComponent implements OnInit {
   items: Items[];
   userId: any;
   Inventories: Inventory[];
+  money:number;
 
-
-  constructor(private adminPanelService: AdminPanelService) { }
+  constructor(
+    private adminPanelService: AdminPanelService,
+    private userStatsService: UserStatsService) { }
 
   ngOnInit(): void {
     this.getItems();
+    
     //this.email = this.adminPanelService.getUserEmail();
     this.userId = this.adminPanelService.getUserId2();
+    this.getUserStats(this.userId);
   }
 
   
@@ -32,6 +37,13 @@ export class ShopComponent implements OnInit {
     var Inventory = { userid:this.userId, itemId:ItemId }
     this.adminPanelService.buyItem(Inventory).subscribe(userId => {
 
+    });
+  }
+
+  getUserStats(userId){
+    this.userStatsService.getUserStat(userId).subscribe(user =>{
+      console.warn(user);
+      this.money = user.money;
     });
   }
 
