c93f8561e053fe8f6efc57cb5abb6bc3f9185c9e
Vincento1111 <vincento2009@hotmail.com>
2023-06-13 06:50:17 +0200

    idkman

--- src/app/Services/UserStats.service.ts
+++ src/app/Services/UserStats.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Input } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
